9116adfd7771d4b61b1c69f5d32be14661968d78
ariadna127 <134458810+ariadna127@users.noreply.github.com>
2025-10-01 12:54:42 -0300

    Update index.js

--- JavaScript/E-COMMERCE2025/client/js/index.js
+++ JavaScript/E-COMMERCE2025/client/js/index.js
@@ -1,11 +1,27 @@
 const shopContent = document.getElementById('shopContent');
+const cart = []; //Este es nuestro carrito
 
 productos.forEach((product) => {
     const content = document.createElement('div');
     content.innerHTML = `
     <img src="${product.img}" alt="">
-    <h3>${product.producName}</h3>
+    <h3>${product.productName}</h3>
     <p>${product.price}</p>
     `;
     shopContent.append(content);
+
+    const buyButton = document.createElement('button');
+    buyButton.innerText = "Comprar";
+    content.append(buyButton);
+
+    buyButton.addEventListener('click', ()=>{
+        cart.push({
+            id : product.id,
+            productName : product.productName,
+            price : product.price,
+            quanty : product.quanty,
+            img : product.img
+        })
+        console.log(cart);
+    })
 })
