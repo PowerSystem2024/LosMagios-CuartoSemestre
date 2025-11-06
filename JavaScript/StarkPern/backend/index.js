2ab82a014d187b57b01f5c3fb98733ebf0f92b80
PabloApa7 <89990249+Pablooxx@users.noreply.github.com>
2025-11-05 21:35:22 -0300

    Delete JavaScript/StarkPern directory

--- JavaScript/StarkPern/backend/index.js
+++
@@ -1,9 +0,0 @@
-import app from './app.js';
-import { PORT } from './config.js';
-import cookieParser from 'cookie-parser';
-
-app.use(cookieParser());
-
-app.listen(PORT);
-console.log('Server on port', PORT);
-
