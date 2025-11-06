25ba35081df2d4016700aeaf3d15bea151ba6f48
PabloApa7 <pabloaparicio996@gmail.com>
2025-11-05 21:41:06 -0300

    StackPern Completo

---
+++ JavaScript/StackPern/backend/router/tareas.routes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { actualizarTarea, crearTarea, eliminarTarea, listarTarea, listarTareas } from "../controllers/tareas.controller.js";
+import {isAuth} from "../middlewares/auth.middleware.js"
+import { validateSchema } from "../middlewares/validateschema.js";
+import { createTareasSchema } from "../schemas/tareas.schema.js";
+import { updateTareasSchema } from "../schemas/tareas.schema.js";
+
+const router = Router();
+
+router.get('/tareas',isAuth, listarTareas );
+
+router.get('/tareas/:id',isAuth, listarTarea);
+
+router.post('/tareas',isAuth, validateSchema(createTareasSchema) ,crearTarea);
+
+router.put('/tareas/:id', isAuth, validateSchema(updateTareasSchema),actualizarTarea);
+
+router.delete('/tareas/:id',isAuth, eliminarTarea);
+
+export default router;
